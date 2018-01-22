b98a738fe0cf0a8fff33990bb50719839bfbaee5
Jeremy Wildsmith <jeremywildsmith@yahoo.ca>
2018-01-21 16:58:16 -0800

    Cleaned up MainMenu state.

--- src/main/java/com/jevaengine/spacestation/gamestates/MainMenu.java
+++ src/main/java/com/jevaengine/spacestation/gamestates/MainMenu.java
@@ -20,12 +20,9 @@ package com.jevaengine.spacestation.gamestates;
 
 import com.jevaengine.spacestation.IState;
 import com.jevaengine.spacestation.IStateContext;
-import com.jevaengine.spacestation.StationProjectionFactory;
 import io.github.jevaengine.FutureResult;
-import io.github.jevaengine.NullIInitializationProgressMonitor;
 import io.github.jevaengine.audio.IAudioClipFactory;
 import io.github.jevaengine.graphics.ISpriteFactory;
-import io.github.jevaengine.math.Vector3F;
 import io.github.jevaengine.ui.Button;
 import io.github.jevaengine.ui.Button.IButtonPressObserver;
 import io.github.jevaengine.ui.IWindowFactory;
@@ -33,15 +30,9 @@ import io.github.jevaengine.ui.IWindowFactory.WindowConstructionException;
 import io.github.jevaengine.ui.NoSuchControlException;
 import io.github.jevaengine.ui.Window;
 import io.github.jevaengine.ui.WindowBehaviourInjector;
-import io.github.jevaengine.ui.WorldView;
-import io.github.jevaengine.util.Nullable;
 import io.github.jevaengine.world.IParallelWorldFactory;
 import io.github.jevaengine.world.IWorldFactory.WorldConstructionException;
 import io.github.jevaengine.world.World;
-import io.github.jevaengine.world.scene.ISceneBufferFactory;
-import io.github.jevaengine.world.scene.TopologicalOrthographicProjectionSceneBufferFactory;
-import io.github.jevaengine.world.scene.camera.ControlledCamera;
-import io.github.jevaengine.world.scene.camera.ICamera;
 import java.net.URI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -49,7 +40,6 @@ import org.slf4j.LoggerFactory;
 public class MainMenu implements IState
 {
 	private static final URI ENTRY_MAP = URI.create("file:///world/derpstation.jmp");
-	private static final URI MENU_WORLD = URI.create("file:///world/derpstation.jmp");
 	
 	private IStateContext m_context;
 	private Window m_window;
@@ -60,9 +50,6 @@ public class MainMenu implements IState
 	private final ISpriteFactory m_spriteFactory;
 	
 	private final Logger m_logger = LoggerFactory.getLogger(MainMenu.class);
-	
-	@Nullable
-	private World m_world = null;
 	
 	public MainMenu(IWindowFactory windowFactory, IParallelWorldFactory worldFactory, IAudioClipFactory audioClipFactory, ISpriteFactory spriteFactory)
 	{
@@ -76,24 +63,10 @@ public class MainMenu implements IState
 	public void enter(IStateContext context)
 	{
 		m_context = context;
-		
-		ISceneBufferFactory sceneBufferFactory = new TopologicalOrthographicProjectionSceneBufferFactory(new StationProjectionFactory().create());
-		ControlledCamera camera = new ControlledCamera(sceneBufferFactory);
-			
+
 		try
 		{
-			m_world = m_worldFactory.getFactory().create(MENU_WORLD, new NullIInitializationProgressMonitor());
-			camera.attach(m_world);
-			camera.lookAt(new Vector3F(4, 4, 0.1F));
-			
-		} catch (WorldConstructionException e)
-		{
-			m_logger.error("Error constructing menu world", e);
-		}
-		
-		try
-		{
-			m_window = m_windowFactory.create(URI.create("file:///ui/windows/mainmenu.jwl"), new MainMenuBehaviourInjector(camera));
+			m_window = m_windowFactory.create(URI.create("file:///ui/windows/mainmenu.jwl"), new MainMenuBehaviourInjector());
 			context.getWindowManager().addWindow(m_window);
 			m_window.center();
 		} catch (WindowConstructionException e)
@@ -115,25 +88,13 @@ public class MainMenu implements IState
 	}
 
 	@Override
-	public void update(int iDelta)
-	{
-		if(m_world != null)
-			m_world.update(iDelta);
-	}
+	public void update(int iDelta) { }
 	
 	public class MainMenuBehaviourInjector extends WindowBehaviourInjector
 	{
-		private final ICamera m_camera;
-		
-		public MainMenuBehaviourInjector(ICamera camera)
-		{
-			m_camera = camera;
-		}
-		
 		@Override
 		public void doInject() throws NoSuchControlException
 		{
-			getControl(WorldView.class, "worldView").setCamera(m_camera);
 			getControl(Button.class, "btnNewGame").getObservers().add(new IButtonPressObserver() {
 				@Override
 				public void onPress() {
