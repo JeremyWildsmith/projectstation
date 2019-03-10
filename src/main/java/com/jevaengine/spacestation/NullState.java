62934e25eb25e9a86809836721e18570c8d85215
Jeremy Wildsmith <jeremywildsmith@yahoo.ca>
2019-03-10 12:54:55 -0700

    Added null state, config item for which world the server uses.

---
+++ src/main/java/com/jevaengine/spacestation/NullState.java
@@ -0,0 +1,18 @@
+package com.jevaengine.spacestation;
+
+public class NullState implements IState {
+    @Override
+    public void enter(IStateContext context) {
+        
+    }
+
+    @Override
+    public void leave() {
+
+    }
+
+    @Override
+    public void update(int deltaTime) {
+
+    }
+}
